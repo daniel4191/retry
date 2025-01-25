d216104813af6defd6d5023467e4a14b7c1af043
daniel4191 <daniell8282@naver.com>
2025-01-25 22:56:28 +0900

    FBV로 블로그 포스트 목록 페이지 제작

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    #  order_by를 -로 해놓으면 역순의 개념이 되는 것이고, 기준값은 pk 즉, 여기서는 생성된 글의 넘버다.
+    posts = Post.objects.all().order_by("-pk")
+
+    return render(
+        request, "blog/index.html",
+        {
+            "posts":posts
+        }
+    )
