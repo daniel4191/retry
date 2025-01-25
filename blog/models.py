bd04b484e423b7d819f2dff899762a664c585573
daniel4191 <daniell8282@naver.com>
2025-01-25 21:57:14 +0900

    Post 모델 생성 및 시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    # auto_now=True를 해주면, 추가로 입력해줄 것 없이, 해당되는 내용이 자동 등록 된다.
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # author - 추후 작성 예정
+
+    # 이걸로써, 관리자 단에서 내용을 보게 되면 작성된 텍스트로 표시된다.
+    def __str__(self):
+        return f"[{self.pk}] {self.title}"
