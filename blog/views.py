1f3f25d602fa4d7aa6475ce1d56baa0b70ade1b5
daniel4191 <daniell8282@naver.com>
2025-01-28 16:41:50 +0900

    내부 구조 변경

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostList(ListView):
@@ -7,6 +7,8 @@ class PostList(ListView):
     ordering = "-pk"
     # template_name = "blog/index.html"
 
+class PostDetail(DetailView):
+    model = Post
 
 # Create your views here.
 # def index(request):
