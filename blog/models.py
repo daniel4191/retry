6f536e0c5a7cdca577691d0fe379f9d2286e26a9
daniel4191 <daniell8282@naver.com>
2025-01-28 18:05:42 +0900

    file_field 추가

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,10 @@ class Post(models.Model):
     content = models.TextField()
 
     # auto_now=True를 해주면, 추가로 입력해줄 것 없이, 해당되는 내용이 자동 등록 된다.
-    head_image = models.ImageField(upload_to="blog/%Y/%m/%d/", blank=True)
+    # settings에 MEDIA_URL, MEDIA_ROOT로 넣어주었던 주소 뒤로 어떻게 해줄지를 말해준다.
+    head_image = models.ImageField(upload_to="blog/images/%Y/%m/%d/", blank=True)
+    file_upload = models.FileField(upload_to="blog/files/%Y/%m/%d/", blank=True)
+
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author - 추후 작성 예정
