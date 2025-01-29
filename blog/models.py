e9d36d1b088b8c3cb36563d08630d50aa5ba8bb4
daniel4191 <daniell8282@naver.com>
2025-01-29 17:00:06 +0900

    포스트 목록의 카테고리 기능 추가, 미완성

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,18 @@ from django.contrib.auth.models import User
 import os
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    # admin 단에서의 이름을 설정
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     # 후킹해주는 메세지 100글자 한도로 노출
@@ -19,7 +31,10 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     # CASCADE는 연결되어있는 값도 같이 삭제 해준다는 뜻
     # SET_NULL은 해당 값을 삭제해도, 해당 pk 값은 공백으로 두되, 나머지 데이터는 살려두는 것
-    author = models.ForeignKey(User, null=True, on_delete = models.SET_NULL)
+    # blank=True를 해줘야 카테고리 미 추가시 오류가 뜨지 않는다.
+    author = models.ForeignKey(User, null=True, blank=True, on_delete = models.SET_NULL)
+    
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
     # 이걸로써, 관리자 단에서 내용을 보게 되면 작성된 텍스트로 표시된다.
     def __str__(self):
