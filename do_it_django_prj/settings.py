10f7881c40effa630c72421026192b70f5e2a1b7
daniel4191 <daniell8282@naver.com>
2025-02-14 14:23:09 +0900

    render

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -62,7 +62,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "allauth.account.middleware.AccountMiddleware"
+    "allauth.account.middleware.AccountMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = 'do_it_django_prj.urls'
@@ -91,8 +92,12 @@ WSGI_APPLICATION = 'do_it_django_prj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('daniel_db'),
+        'USER': os.environ.get('daniel'),
+        'PASSWORD': os.environ.get('logisWkd1'),
+        'HOST': os.environ.get('daniel_db'),
+        'PORT': os.environ.get('8000', '5432'),
     }
 }
 
